63d15ac93790191fe67fa56acc60ef79e7812dd9
enforceway <enforceway@163.com>
2018-01-10 00:54:57 +0800

    添加sql文件

---
+++ mysql/table_creation.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `hello_contact`.`contact_t` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `mobile_number` VARCHAR(45) NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+
