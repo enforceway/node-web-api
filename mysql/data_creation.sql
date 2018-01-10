c00e9e4c0f4142af065d105d42be0e2e2ca10640
enforceway <enforceway@163.com>
2018-01-10 11:06:32 +0800

    data creation sql file added.

---
+++ mysql/data_creation.sql
@@ -0,0 +1,3 @@
+INSERT INTO `hello_contact`.`contact_t` (`id`, `mobile_number`, `name`) VALUES ('1', '1008611', 'enforceway');
+INSERT INTO `hello_contact`.`contact_t` (`id`, `mobile_number`, `name`) VALUES ('2', '1008610', 'lizhaolongamd');
+INSERT INTO `hello_contact`.`contact_t` (`id`, `mobile_number`, `name`) VALUES ('3', '10086', 'worldexpress');
