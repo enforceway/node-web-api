c940b6b09280dc9e4f9e7b4d87448843e06d356e
enforceway <enforceway@163.com>
2018-01-10 17:54:36 +0800

    完善跨域

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -29,9 +30,14 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "origin,  X-Custom-Header, X-Requested-With, content-type, accept, authorization");
+	res.header("Access-Control-Allow-Credentials", "true");
+	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+	res.header("Access-Control-Max-Age", "1209600");
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // error handler
