260913fb4c31fb998e61d7d3e095b6ccf648193a
enforceway <enforceway@163.com>
2018-01-11 17:36:51 +0800

    new api added

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var dbConnector = require("../util/mysql-ping");
+var newResponse = require("../util/responseGenerator");
 
 /* GET home page. */
 // router.get('/', function(req, res, next) {
@@ -30,5 +31,32 @@ router.get('/api/getContactList', function(req, res, next) {
 	});
 	connectInst.end();
 });
+
+router.get('/api/getContactById', function(req, res, next) {
+	var responseObj = newResponse();
+	if(!req.query.id) {
+		responseObj.ifSuccess = false;
+		// responseObj.code = '';
+		res.json(responseObj);
+		return;
+	}
+
+	var connectInst = dbConnector();
+	connectInst.connect();
+	connectInst.query('select id, name, mobile_number, address, email, birthday, description from contact_t where id = ?',[req.query.id], function(error, results, fields) {
+		if(error) {
+			responseObj.ifSuccess = false;
+			responseObj.msg = error;
+			res.json(responseObj);
+			return;
+        }
+        if(results.length == 1) {
+        	responseObj.data = results[0];	
+        }
+        res.json(responseObj);
+	});
+	connectInst.end();
+});
+
 
 module.exports = router;
