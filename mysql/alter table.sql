c8171e087ec44961ed08d946401036abe6ad0eb1
enforceway <enforceway@163.com>
2018-01-11 17:34:27 +0800

    alter table

--- mysql/alter table.sql
+++ mysql/alter table.sql
@@ -2,3 +2,4 @@ ALTER TABLE `hello_contact`.`contact_t`
 ADD COLUMN `address` VARCHAR(500) NULL AFTER `name`,
 ADD COLUMN `email` VARCHAR(100) NULL AFTER `address`,
 ADD COLUMN `birthday` DATE NULL AFTER `email`;
+ADD COLUMN `description` VARCHAR(500) NULL AFTER `birthday`;
