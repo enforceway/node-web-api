b70e5639ba0a5ed902bf683c2609a5aea674c091
enforceway <enforceway@163.com>
2018-01-11 18:50:56 +0800

    new api added

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var dbConnector = require("../util/mysql-ping");
 var newResponse = require("../util/responseGenerator");
+var request = require('request');
 
 /* GET home page. */
 // router.get('/', function(req, res, next) {
@@ -27,7 +28,6 @@ router.get('/api/getContactList', function(req, res, next) {
 
         responseObj.data = results;
         res.json(responseObj);
-        console.log(results);
 	});
 	connectInst.end();
 });
@@ -55,6 +55,40 @@ router.get('/api/getContactById', function(req, res, next) {
         }
         res.json(responseObj);
 	});
+	connectInst.end();
+});
+
+router.post('/api/updateContact', function(req, res, next) {
+	var responseObj = newResponse();
+	console.log("updateContact:", req.body);
+	if(!req.body.id) {
+		responseObj.ifSuccess = false;
+		// responseObj.code = '';
+		res.json(responseObj);
+		return;
+	}
+
+	var connectInst = dbConnector();
+	connectInst.connect();
+	console.log("updateContact");
+	request('/api/getContactById?id=' + req.body.id, function (error, response, body) {
+		console.log('error:', error); // Print the error if one occurred
+		console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+		console.log('body:', body); // Print the HTML for the Google homepage.
+	});
+	res.json(responseObj);
+	// connectInst.query('select id, name, mobile_number, address, email, birthday, description from contact_t where id = ?',[req.query.id], function(error, results, fields) {
+	// 	if(error) {
+	// 		responseObj.ifSuccess = false;
+	// 		responseObj.msg = error;
+	// 		res.json(responseObj);
+	// 		return;
+ //        }
+ //        if(results.length == 1) {
+ //        	responseObj.data = results[0];	
+ //        }
+ //        res.json(responseObj);
+	// });
 	connectInst.end();
 });
 
